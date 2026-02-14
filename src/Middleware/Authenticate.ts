286b3ea9474f8c527290b5d581e0a708e7d12a58
Prakash Tank <prakashtank106@gmail.com>
2026-02-14 23:51:43 +0530

    Refactor: Update Authenticate middleware to use new signature

--- src/Middleware/Authenticate.ts
+++ src/Middleware/Authenticate.ts
@@ -1,22 +1,35 @@
 import { AuthManager } from '../AuthManager';
 
 export class Authenticate {
+    protected guards: string[] = [];
+
     constructor(private auth: AuthManager) { }
 
-    public async handle(request: any, next: () => Promise<any>, ...guards: string[]): Promise<any> {
+    /**
+     * Set the guards that should be checked.
+     */
+    public using(...guards: string[]): this {
+        this.guards = guards;
+        return this;
+    }
+
+    /**
+     * Handle the incoming request.
+     */
+    public async handle(request: any, next: (request: any) => Promise<any> | any): Promise<any> {
         // 1. Bind the current request to the AuthManager/Guards
         this.auth.setRequest(request);
 
         // 2. Determine guards to check
-        if (guards.length === 0) {
-            guards = [undefined as unknown as string]; // Use default guard
-        }
+        const guardsToCheck = this.guards.length === 0
+            ? [undefined as unknown as string]
+            : this.guards;
 
         // 3. Check each guard
-        for (const guard of guards) {
+        for (const guard of guardsToCheck) {
             if (await this.auth.guard(guard).check()) {
                 this.auth.shouldUse(guard);
-                return next();
+                return next(request);
             }
         }
 
